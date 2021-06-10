c9850943526c425965ecda65fd59b55235516474
im-amit-kumar <67156610+im-amit-kumar@users.noreply.github.com>
2021-06-10 11:07:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def predict():
     return render_template('index.html', prediction_text='{}'.format(output))
 
 
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
